2e34aa0508e811a722e95b05f6155f936f873edf
zucktang <zucktang@gmail.com>
2024-01-02 22:39:16 +0700

    list_c

--- list_c.py
+++ list_c.py
@@ -8,21 +8,31 @@ def generate_c(a, b):
         found = False 
         for x in range(N):
             for y in range(x, N):
-               
+    
                 z = b[i] - a[x] - a[y]
+                print(f"{z} = {b[i]} - {a[x]} - {a[y]}")
+                # print(f"i: {i}  || b[i]: {b[i]}")
+                # print(f"x: {x}  || a[x]: {a[x]}")
+                # print(f"y: {y}  || {a[y]}: {{a[y]}}")
+                # print(a)
+                # print(b)
+                # print("set_a: ", set_a)
                 if z in set_a:
                     list_c[i] = 1
                     found = True
-                    print(f"{a[x]} + {a[y]} + {z} = {b[i]}")
+                    # print(f"{a[x]} + {a[y]} + {z} = {b[i]}")
+                    print(found)
                     break
+                print(found)
+                
             if found:
-                break 
+                break
 
     return list_c
 
 a = [5, 2, 3, 4, 10, 11]
-b = [5, 7, 26, 16, 5, 10, 12]
+b = [5, 7, 26]
 result_c = generate_c(a, b)
 print(result_c)
 
-time complexity = O(MN^2)
+# time complexity = O(MN^2)
